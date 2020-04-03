e3ea87135142404450c3157fa153403fdf16c4c7
Mingyu Li <lmy2010lmy@gmail.com>
2020-04-03 22:13:52 +0800

    multi-threaded test

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import socket, ssl
+import threading
+import time
+from timeit import default_timer as timer
 
 HOST, PORT = '127.0.0.1', 1234
 
 def handle(conn):
     conn.write(b'GET / HTTP/1.1\n')
-    print(conn.recv().decode())
+    # print(conn.recv().decode())
+    conn.recv().decode()
 
-def main():
+def https_client(port):
     sock = socket.socket(socket.AF_INET)
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     context.load_cert_chain(certfile="client.crt", keyfile="client.key")
@@ -15,10 +19,31 @@ def main():
     ssl.match_hostname = lambda cert, hostname: hostname == cert['subjectAltName'][0][1]
     conn = context.wrap_socket(sock, server_hostname=HOST)
     try:
-        conn.connect((HOST, PORT))
+        conn.connect((HOST, port))
         handle(conn)
     finally:
         conn.close()
 
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, port):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.port = port
+    def run(self):
+        https_client(self.port)
+
 if __name__ == '__main__':
-    main()
+    threads = []
+    tic = timer()
+    
+    for x in xrange(1,10):
+        thread = myThread(x, "HTTPS-SERVER", 9000 + x)
+        thread.start()
+        threads.append(thread)
+    
+    for t in threads:
+        t.join()
+
+    toc = timer()
+    print(toc - tic)
--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 import socket, ssl
+import threading
+import time
 
 HOST, PORT = '127.0.0.1', 1234
 
-def handle(conn):
-  print(conn.recv())
-  """
-  put the kms signing process here
-  """
-  conn.write(b'HTTP/1.1 200 OK\n\n%s' % conn.getpeername()[0].encode())
+frame = 'w' * 1500 * 10
 
-def main():
+def handle(conn):
+  # print(conn.recv())
+  conn.write(b'HTTP/1.1 200 OK%s\n\n%s' % (frame, conn.getpeername()[0].encode()))
+
+def https_server(port):
   sock = socket.socket()
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  sock.bind((HOST, PORT))
+  sock.bind((HOST, port))
   sock.listen(5)
   context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
   context.load_cert_chain(certfile="server.crt", keyfile="server.key")  # 1. key, 2. cert, 3. intermediates
@@ -30,5 +31,17 @@ def main():
     finally:
       if conn:
         conn.close()
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, port):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.port = port
+    def run(self):
+        https_server(self.port)
+
 if __name__ == '__main__':
-  main()
+  for x in xrange(1,10):
+    thread = myThread(x, "HTTPS-SERVER", 9000 + x)
+    thread.start()
